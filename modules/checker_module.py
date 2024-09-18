3016ce5d423794bf26cf23490d798985706f9b47
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-19 00:44:32 +0500

    Update checker_module.py

--- modules/checker_module.py
+++ modules/checker_module.py
@@ -1,10 +1,9 @@
 import asyncio
 from aiohttp import ClientResponseError
 from loguru import logger
-from web3 import AsyncWeb3
+from web3 import Web3
 import random
-
-import random
+from concurrent.futures import ThreadPoolExecutor
 
 from utils.config import RPC
 from settings import DECIMALS, PROXY
@@ -14,11 +13,19 @@ class BalanceChecker:
     def __init__(self, address: str):
         self.chains = ['ethereum', 'zksync', 'arbitrum', 'optimism', 'scroll', 'base']
         self.address = address
-    
+        self.executor = ThreadPoolExecutor(max_workers=len(self.chains))  # Define the thread pool
+
     async def run_checker(self):
         try:
-            tasks = [asyncio.create_task(self.check_balance(chain)) for chain in self.chains]
+            loop = asyncio.get_event_loop()
 
+            # Run check_balance concurrently in threads
+            tasks = [
+                loop.run_in_executor(self.executor, self.check_balance, chain)
+                for chain in self.chains
+            ]
+
+            # Gather the results
             balances = await asyncio.gather(*tasks)
             
             sum_balances = sum(balances)
@@ -31,17 +38,17 @@ class BalanceChecker:
         except Exception as e:
             logger.error(f'{self.address} | Unexpected error: {e}')
     
-    async def check_balance(self, chain):
+    def check_balance(self, chain):
         request_kwargs = {'proxy': f'http://{random.choice(PROXY)}'} if PROXY else {}
-        w3 = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(random.choice(RPC[chain]['rpc'])), request_kwargs=request_kwargs)
+        w3 = Web3(Web3.HTTPProvider(random.choice(RPC[chain]['rpc'])), request_kwargs=request_kwargs)
 
         try:
-            wei_balance = await w3.eth.get_balance(w3.to_checksum_address(self.address))
+            wei_balance = w3.eth.get_balance(w3.to_checksum_address(self.address))
         except ClientResponseError:
             logger.error('Too many requests. Sleep 5 sec.')
-            await asyncio.sleep(5)
-            return await self.check_balance(chain)
-        
+            asyncio.sleep(5)  # Although this is running in a thread, we still use asyncio.sleep
+            return self.check_balance(chain)
+
         eth_balance = round(w3.from_wei(wei_balance, 'ether'), DECIMALS)
 
         return eth_balance
