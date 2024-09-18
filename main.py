eb1944fa2dd37948358e44b1a2e83e092124211a
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-19 01:08:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from loguru import logger
+
 from modules.checker_module import BalanceChecker 
 from utils.utils import get_address_list, setup_balances_to_write
 
@@ -8,18 +10,25 @@ async def run_main(max_threads: int = None):
 
     tasks = []
 
+    # Loop through each address and create a BalanceChecker instance
     for address in address_list:
         balance_checker = BalanceChecker(address, max_threads=max_threads)
         tasks.append(asyncio.create_task(balance_checker.run_checker()))
 
+    # Await all balance checks
     balances = await asyncio.gather(*tasks)
 
-    setup_balances_to_write(balances)
+    # Write the balances to the text file
+    setup_balances_to_write(balances, address_list)
 
-    logger.success(f'All balances have been successfully written to wallets.xlsx!')
+    logger.success(f'All balances have been successfully written to wallet_balances.txt!')
 
 
+# Configure logging to file
 logger.add('logs.log')
 
-NUMBER_OF_THREADS = 4
+# Define the number of threads to use for balance checking
+NUMBER_OF_THREADS = 15
+
+# Run the main function
 asyncio.run(run_main(max_threads=NUMBER_OF_THREADS))
