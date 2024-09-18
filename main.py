defafaf3dd4ad560004993bcdf66f9044f5dc143
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-19 00:56:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import asyncio
-
-from loguru import logger
-
-from modules.checker_module import BalanceChecker
-from utils.utils import get_address_list, setup_balances_to_write
-
-
 async def run_main():
-    address_list = get_address_list()
-    
+    address_list = get_address_list()  # Get the list of addresses to check
+
     tasks = []
-    
+
+    # Loop through each address and create a BalanceChecker instance
     for address in address_list:
         balance_checker = BalanceChecker(address)
         tasks.append(asyncio.create_task(balance_checker.run_checker()))
-            
+
+    # Await all balance checks
     balances = await asyncio.gather(*tasks)
-    
+
+    # Setup the final results to be written (presumably to an Excel file)
     setup_balances_to_write(balances)
-    
+
     logger.success(f'All balances have been successfully written to wallets.xlsx!')
 
+
+# Add logging to file and run the main function
 logger.add('logs.log')
 asyncio.run(run_main())
